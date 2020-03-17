bab54e034585cf241c5dc3c46b51bcc281221733
lmonty22 <lindsay.montgomery@olympian.org>
2020-03-17 14:34:29 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+
+INSERT INTO bears (name, age,gender, color, temperament, alive) 
+    VALUES ("Mr.Chocolate", 5, "Male", "Brown", "Calm", TRUE);
+
+INSERT INTO bears (name,age,gender,color,temperament, alive)
+    VALUES ("Rowdy", 8, "Female", "Pink", "Rowdy", FALSE);
+
+INSERT INTO bears (name,age,gender,color,temperament, alive)
+    VALUES ("Tabitha", 2, "Female", "White", "Angry", TRUE);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+    VALUES ("Sergeant Brown", 5, "Male", "Balck", "Calm", FALSE);
+
+INSERT INTO bears (name,age, gender, color, temperament, alive)
+    VALUES ("Melissa", 3, "Female", "While", "Calm", TRUE);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 6, "Male", "Green", "Bitter", TRUE);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Wendy", 9, "Female","Red", "Hungry", FALSE);
+
+INSERT INTO bears (age, gender, color, temperament, alive)
+    VALUES (6, "Female", "Black", "Sad", TRUE);
