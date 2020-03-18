2a101d14ffbf34261db33195611c573eae3b22d3
kaidornel <kai_dornel@hotmail.com>
2020-03-18 19:32:09 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 56, "M", "Black", "Happy", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 56, "M", "Black", "Happy", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 56, "M", "Black", "Happy", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 56, "M", "Black", "Happy", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 56, "M", "Black", "Happy", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 56, "M", "Black", "Happy", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 56, "M", "Black", "Happy", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, null, 56, "M", "Black", "Happy", true);
+
+
+
+-- CREATE TABLE bears (
+--     id INTEGER PRIMARY KEY, 
+--     name TEXT,
+--     age INTEGER,
+--     gender TEXT,
+--     color TEXT,
+--     temperament TEXT,
+--     alive BOOLEAN
+-- );
+
+-- INSERT INTO cats (id, name, age, breed, net_worth) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
