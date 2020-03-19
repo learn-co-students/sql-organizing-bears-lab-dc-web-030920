998cfd48b0176787ac183002e762cfc1b5ef9bf7
musiccity365 <m_sackstein@yahoo.com>
2020-03-19 12:36:12 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears
+    (id, name, age, gender, color, temperament, alive)
+VALUES
+    (1, 'Mr. Chocolate', 6, 'M', 'brown', 'calm', FALSE),
+    (2, 'Rowdy', 2, 'M', 'black', 'intense', TRUE),
+    (3, 'Tabitha', 4, 'F', 'brown', 'fine', TRUE),
+    (4, 'Sergeant Brown', 10, 'M', 'brown', 'slimy', FALSE),
+    (5, 'Melissa', 3, 'F', 'black', 'goofy', TRUE),
+    (6, 'Grinch', 8, 'M', 'black', 'coarse', FALSE),
+    (7, 'Wendy', 5, 'F', 'black', 'naive', TRUE),
+    (8, NULL, 9, 'M', 'black', 'aggressive', FALSE);
